4e86dc7835eae06cdda11b539cab6fadc87e0e15
Jay Desai <jaydesai571@gmail.com>
2020-08-21 00:28:44 -0400

    React-Router add with some sample stateless functional components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
 
-ReactDom.render(<p>Boiler Plate</p>,document.getElementById('app'));
+const AddExpensePage = () => (
+    <div>
+        This is my Add component
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is from Edit component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from the Help component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDom.render(routes,document.getElementById('app'));

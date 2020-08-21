d6fe48780c36d4e21194e74dc5d3bb26fe8c02ac
Jay Desai <jaydesai571@gmail.com>
2020-08-20 23:58:31 -0400

    Project Create wit all basic setup of REACT

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDom.render(<p>Boiler Plate</p>,document.getElementById('app'));

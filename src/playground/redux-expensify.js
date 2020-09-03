10dd100914b128cc2572edf59d5bbe9b3ca56d5a
Jay Desai <jaydesai571@gmail.com>
2020-09-03 00:18:15 -0400

    All parameters filters add

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -131,8 +131,8 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
         const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch;
-        const textMatch;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
     });
@@ -146,23 +146,25 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100}));
-// const expenseTwo = store.dispatch(addExpense({description: 'Utilites Bill', amount: 10}));
+const expenseOne = store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: 1000}));
+const expenseTwo = store.dispatch(addExpense({description: 'Utilites Bill', amount: 10, createdAt: -1000}));
 
 // store.dispatch(removeExpense({id:expenseOne.expense.id}));
 // store.dispatch(editExpense(expenseTwo.expense.id, {amount: 500}));
-// store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
+// store.dispatch(setStartDate(-2000));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(1250));
 
 const demoState = {
     expenses: [{
